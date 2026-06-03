8a8ce2e3a2566ce61e0e39b465ddfe86100e5fec
Lucas Alves <167643716+Luskahz@users.noreply.github.com>
2026-06-03 12:39:26 -0300

    feat: register authorization roles route

--- src/features/navigation/lib/appRoutes.js
+++ src/features/navigation/lib/appRoutes.js
@@ -6,6 +6,7 @@ export const APP_ROUTES = Object.freeze({
   FORGOT_PASSWORD: "/forgot-password",
   PASSWORD_RECOVERY_RESET: "/password-recovery/reset",
   DPO: "/dpo",
+  AUTHORIZATION_ROLES: "/authorization/roles",
   SERVER_MANAGER: "/servidor",
 });
 
